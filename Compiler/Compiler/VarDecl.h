76c4e3d1529ba0e60ba37504d19d4f289e56111b
unknown <alenarykunova@gmail.com>
2015-10-20 15:55:09 +0300

    IHost

--- Compiler/Compiler/VarDecl.h
+++ Compiler/Compiler/VarDecl.h
@@ -5,7 +5,7 @@ class CVarDecl : public IVarDecl
 public:
 	CVarDecl( IType* _type, std::string _id  );
 	~CVarDecl();
-	void Accept( IVisitor* ) const;
+//	void Accept( IVisitor* ) const;
 private:
 	IType* type;
 	std::string id;
