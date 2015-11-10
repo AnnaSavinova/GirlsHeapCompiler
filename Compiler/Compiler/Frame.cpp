94c9c68e990187d201ea6242af855d6ff16d282e
AnnaSavinova <anna.savinova@phystech.edu>
2015-11-10 16:18:03 +0300

    Frames

---
+++ Compiler/Compiler/Frame.cpp
@@ -0,0 +1,34 @@
+#include "Frame.h"
+
+CFrame::CFrame( const CSymbol * name, int formalsCount )
+{}
+
+int CFrame::FormalsCount() const
+{
+    return formals.size();
+}
+
+const IAccess * CFrame::Formal( size_t index ) const
+{
+    return nullptr;
+}
+
+CLabel::CLabel() : name( std::to_string( nextUniqueId++ ) )
+{}
+
+CLabel::CLabel( const std::string& s ) : name( s )
+{}
+
+const std::string & CLabel::Name() const
+{
+    return name;
+}
+
+CTemp::CTemp() : name( std::to_string( nextUniqueId++ ) )
+{}
+
+CTemp::CTemp( const CSymbol * symbol ) : name( symbol->String() )
+{}
+
+CTemp::~CTemp()
+{}
---
+++ Compiler/Compiler/Frame.h
@@ -0,0 +1,58 @@
+#pragma once
+#include <vector>
+#include "Symbol.h"
+class IAccess {
+public:
+    virtual ~IAccess() {}
+};
+
+class CFrame {
+public:
+    CFrame( const CSymbol* name, int formalsCount );
+    //Доступ к формальным параметрам
+    int FormalsCount() const;
+    const IAccess* Formal( size_t index ) const;
+private:
+    std::vector< IAccess* > formals;
+};
+
+//Метка - точка перехода в коде
+class CLabel {
+public:
+    // Создать метку с уникальным именем
+    CLabel();
+    // Создать метку с заданным именем
+    explicit CLabel( const std::string& );
+
+    const std::string& Name() const;
+
+private:
+    // Счётчик для создания уникальных идентификаторов
+    static int nextUniqueId;
+    std::string name;
+
+};
+
+int CLabel::nextUniqueId = 0;
+
+// Временная переменная
+class CTemp {
+public:
+    // Новая переменная с уникальным именем
+    CTemp();
+    // Новая переменная с заданным именем
+    explicit CTemp( const CSymbol* symbol );
+    ~CTemp();
+
+    const std::string& Name() const { return name; }
+
+private:
+    // Счётчик для создания уникальных имён
+    static int nextUniqueId;
+    std::string name;
+};
+
+int CTemp::nextUniqueId = 0;
+
+
+
