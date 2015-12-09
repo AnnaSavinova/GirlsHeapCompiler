c66269aa8225e7f22581db2d3a623f02dee5ac51
paulin-mipt <paulin.mipt@gmail.com>
2015-12-09 08:40:30 +0300

    decllists

--- Compiler/Compiler/IRSeq.cpp
+++ Compiler/Compiler/IRSeq.cpp
@@ -3,5 +3,11 @@
 CIRSeq::CIRSeq( IIRStm * _left, IIRStm * _right ) : left( _left ), right( _right )
 {}
 
+CIRSeq::CIRSeq( std::list<IIRStm*> list ) : left(list.front())
+{
+    list.pop_front();
+    right = new CIRSeq( list );
+}
+
 CIRSeq::~CIRSeq()
 {}
