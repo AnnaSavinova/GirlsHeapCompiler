502e626b9863a5cd025f21268ee8940a5dba09f1
Arlenka <alenarykunova@gmail.com>
2016-03-16 17:10:53 +0300

    +1 file to resolve linking errors

---
+++ Compiler/Compiler/Temp.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "Symbol.h"
+
+// Временная переменная
+class CTemp
+{
+public:
+    // Новая переменная с уникальным именем
+    CTemp();
+    // Новая переменная с заданным именем
+    explicit CTemp( const CSymbol* symbol );
+    ~CTemp();
+
+    const std::string& Name() const { return name; }
+
+private:
+    // Счётчик для создания уникальных имён
+    static int nextUniqueId;
+    std::string name;
+};
+
+//Метка - точка перехода в коде
+class CLabel
+{
+public:
+    // Создать метку с уникальным именем
+    CLabel();
+    // Создать метку с заданным именем
+    explicit CLabel( const std::string& );
+
+    const std::string& Name() const;
+
+private:
+    // Счётчик для создания уникальных идентификаторов
+    static int nextUniqueId;
+    std::string name;
+
+};
